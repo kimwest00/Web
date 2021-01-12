afb8758d64598a929a07ee1d3a16f0eebf22fe4b
김민서 <kim-west00@naver.com>
2021-01-13 06:54:39 +0900

    state활용_value값을 state값으로 설정

--- EventPractice.js
+++ EventPractice.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 
 class EventPractice extends Component {
+    state={
+        message: ' '
+    }
     render(){
         return(
             <div>
@@ -9,10 +12,13 @@ class EventPractice extends Component {
                 type="text"
                 name="message"
                 placeholder="아무거나 입력하렴"
+                value ={this.state.message}
                 onChange={
                     (e) => {
-                        console.log(e.target.value);//SynthticEvent_이벤트가 종료하면 이벤트 초기화_정보참조불가
-                    }//e.target.value로 log괄호값을 수정해주면 바뀌는값이 기록된다
+                        this.setState({
+                            message: e.target.value
+                        })
+                    }
                 }
                 />
             </div>
