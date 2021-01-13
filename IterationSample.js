ecc98fc9e6db2227a5424ca3965302443f337af1
김민서 <kim-west00@naver.com>
2021-01-13 09:12:50 +0900

    데이터배열을 컴포넌트 배열로 변환하기

--- IterationSample.js
+++ IterationSample.js
@@ -1,18 +1,12 @@
 import React from  'react';
 
 const IterationSample =() =>{
-    return(
-        <u1>
-            <li>눈사람</li>
-            <li>얼음</li>
-            <li>눈</li>
-            <li>바람</li>
-
-
-
-
-        </u1>
-    );
+    const names=['눈사람', '얼음', '눈', '바람'];
+    const namesList = names.map((name,index)=><li key={index}>{name}</li>);
+    //const nameList=names.map(name => <li>{name}</li>);
+    //key가 없지만 실행은 가능함,콘솔에서 경고메세지만 나올뿐
+    
+    return<u1>{namesList}</u1>;
 
 };
 export default IterationSample;
