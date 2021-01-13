62212a710ee5617c299a8b631fdcf34ed3d3bbed
김민서 <kim-west00@naver.com>
2021-01-13 09:06:28 +0900

    반복되는코드

---
+++ IterationSample.js
@@ -0,0 +1,18 @@
+import React from  'react';
+
+const IterationSample =() =>{
+    return(
+        <u1>
+            <li>눈사람</li>
+            <li>얼음</li>
+            <li>눈</li>
+            <li>바람</li>
+
+
+
+
+        </u1>
+    );
+
+};
+export default IterationSample;
